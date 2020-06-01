8f7806215c618cc0b7ab3c40236d7a03e8f7c985
minhtn143 <minhtn143@gmail.com>
2020-06-01 15:28:49 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('create', 'PostController@create');
+Route::post('store', 'PostController@store')->name('store');
+Route::get('custom-validation', 'FormController@checkValidation')->name('form.submit');
